4b93bd5dfceec4c8566ba9ba739c647b983e045f
oswi <oswaldo.adrian@live.com.mx>
2022-12-21 18:50:08 -0700

    jdbc user details manager

--- src/main/java/dev/ososuna/eazybank/configuration/SecurityConfiguration.java
+++ src/main/java/dev/ososuna/eazybank/configuration/SecurityConfiguration.java
@@ -1,8 +1,14 @@
 package dev.ososuna.eazybank.configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,4 +24,15 @@ public class SecurityConfiguration {
       .and().httpBasic();
     return http.build();
   }
+
+  @Bean
+  UserDetailsService userDetailsService(DataSource dataSource) {
+    return new JdbcUserDetailsManager(dataSource);
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 }
