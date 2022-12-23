4a85aaebe49de8570aad561b7558470366dc5f6f
oswi <oswaldo.adrian@live.com.mx>
2022-12-23 14:37:27 -0700

    custom authentication provider

---
+++ src/main/java/dev/ososuna/eazybank/configuration/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,42 @@
+package dev.ososuna.eazybank.configuration;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import dev.ososuna.eazybank.model.Customer;
+import dev.ososuna.eazybank.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+  private final PasswordEncoder passwordEncoder;
+  private final CustomerRepository customerRepository;
+
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    final String email = authentication.getName();
+    final String password = authentication.getCredentials().toString();
+    final Optional<Customer> customerOptional = customerRepository.findByEmailAndActiveTrue(email);
+    if (customerOptional.isPresent()) {
+      final Customer customer = customerOptional.get();
+      if (passwordEncoder.matches(password, customer.getPassword())) {
+        return new UsernamePasswordAuthenticationToken(email, password, List.of(() -> customer.getRole()));
+      }
+    }
+    throw new BadCredentialsException("Invalid username or password");
+  }
+
+  @Override
+  public boolean supports(Class<?> authentication) {
+    return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+  }
+  
+}
