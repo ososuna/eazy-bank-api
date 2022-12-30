3f3a3ac8e6d882764f54bc4d914dff6ffa4abc28
oswi <oswaldo.adrian@live.com.mx>
2022-12-30 14:25:02 -0700

    exception controller advice

---
+++ src/main/java/dev/ososuna/eazybank/exception/ExceptionControllerAdvice.java
@@ -0,0 +1,22 @@
+package dev.ososuna.eazybank.exception;
+
+import java.net.URISyntaxException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+  @ExceptionHandler(BadRequestException.class)
+  public ProblemDetail handleBadRequestException(BadRequestException e, HttpServletRequest request) throws URISyntaxException {
+    final ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+    problemDetail.setTitle("Bad Request");
+    return problemDetail;
+  }
+
+}
